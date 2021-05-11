e8384770fb8e30dccd379b288a72079ba01030d9
hewittaj <hewittaj@outlook.com>
2021-05-11 18:38:53 -0500

    working on time functionality

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ total_distance = 0.0  # Sum of all the distances a truck traveled
 
 # Manually load some packages
 be_together = [13, 14, 15, 16, 19, 20, 1, 28, 29, 30, 34, 39]  # Packages that must be delivered together on truck one
-only_truck_two = [3, 18, 35, 37, 38, 27, 28, 32, 33, 36, 40]  # Packages that can only be on truck two
-delayed = [6, 25, 31]  # Packages that are delayed and won't arrive until 9:05 go to truck two
+only_truck_two = [3, 18, 35, 37, 38, 27, 31, 33, 36, 40]  # Packages that can only be on truck two
+delayed = [6, 25, 28, 32]  # Packages that are delayed and won't arrive until 9:05 go to truck two
 no_special_needs = [2, 4, 5, 7, 8, 9, 10, 11, 12, 17, 19, 21, 22, 23, 24, 26]  # Packages that don't have special needs
 
 # Manually sort packages
@@ -35,22 +35,35 @@ for i in range(1, 41):
     # Load packages that have to be delivered together on truck 1
     if int(package_info[0]) in be_together:
         truck1.append(package_info)
+        truck1[-1][10] = '8:00'
 
     # Load packages that can only be on truck two
     if int(package_info[0]) in only_truck_two:
         truck2.append(package_info)
+        truck2[-1][10] = '9:05'
 
     # Load packages that are delayed to truck two
     if int(package_info[0]) in delayed:
         truck2.append(package_info)
+        truck2[-1][10] = '9:05'
 
     # Load packages with no special needs to truck 3. Will be full after this
     if int(package_info[0]) in no_special_needs:
+        # Update package w/ wrong address since it is on our reserve truck time will have passed
+        # for it to be corrected
+        if package_info[0] == '9':
+            package_info[1] = '410 S State St'
+            package_info[2] = 'Salt Lake City'
+            package_info[3] = 'UT'
+            package_info[4] = '84111'
         truck3.append(package_info)
+        truck3[-1][10] = '8:00'
 
-greedy_route_truck1.insert(0, ['0', '4001 South 700 East'])  # Insert 0 as they all start at the hub
-greedy_route_truck2.insert(0, ['0', '4001 South 700 East'])  # Insert 0 as they all start at the hub
-greedy_route_truck3.insert(0, ['0', '4001 South 700 East'])  # Insert 0 as they all start at the hub
+
+# Insert 0 as they all start at the hub
+greedy_route_truck1.insert(0, ['0', '4001 South 700 East', '', '', '', '', '', '', '', '', '8:00'])
+greedy_route_truck2.insert(0, ['0', '4001 South 700 East', '', '', '', '', '', '', '', '', '9:05'])
+greedy_route_truck3.insert(0, ['0', '4001 South 700 East', '', '', '', '', '', '', '', '', '8:00'])
 
 while truck1:
     distance_info = d.get_greedy_route(truck1, greedy_route_truck1)  # Get distance info for the next truck
@@ -60,10 +73,10 @@ while truck1:
 
     # If the last package has gone out, then we need to return to the hub
     if len(truck1) == 0:
-        truck1.append(['0', '4001 South 700 East'])  # Set truck to return to hub
+        truck1.append(['0', '4001 South 700 East', '', '', '', '', '', '', '', '', '8:00'])  # Set truck to return to hub
         distance_info = d.get_greedy_route(truck1, greedy_route_truck1)  # Get distance info for the next truck
         truck1_distance_info.append(distance_info)  # Append to our list for our later time calculations
-        greedy_route_truck1.append(['0', '4001 South 700 East'])  # Add package to greedy_route
+        greedy_route_truck1.append(['0', '4001 South 700 East', '', '', '', '', '', '', '', '', '8:00'])  # Add package to greedy_route
         break  # Done with this loop
 distance_info = []  # Clear out info
 
--- time_calculations.py
+++ time_calculations.py
@@ -3,8 +3,23 @@ import datetime
 
 # Calculate the time needed for each delivery on a truck
 def calculate_time(distance_info, truck_list):
+    adjusted_time = ''
+    count = 0
     for info in distance_info:
         calculated_time = info[0] / 18  # Calculate time traveled by using avg speed of truck
-        calculated_minutes = '{0:02.0f}:{1:02.0f}'.format(
-            *divmod(calculated_time * 60, 60))
-        print(f"{calculated_minutes}")
+        calculated_minutes = '{0:02.0f}:{1:02.0f}'.format(*divmod(calculated_time * 60, 60))  # Calculate minutes
+        #print(truck_list[0][10])
+        adjusted_time = datetime.timedelta()  # Our time that will be adjusted by
+        prior_time = datetime.timedelta()
+        (prior_hour, prior_minute) = truck_list[count][10].split(":")
+        prior_time = datetime.timedelta(hours=int(prior_hour), minutes=int(prior_minute))
+        (hour, minute) = calculated_minutes.split(":")
+        adjusted_time += datetime.timedelta(hours=int(hour), minutes=int(minute))
+        print(adjusted_time)
+        if count == 0:
+            prior_time += datetime.timedelta(hours=int(hour), minutes=int(minute))
+            truck_list[count + 1][9] = str(prior_time)
+
+        count += 1
+
+    return truck_list
